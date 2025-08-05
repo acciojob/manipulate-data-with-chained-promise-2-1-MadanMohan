b04db8f8a7eaf4a3de7ae6aaf71425c4b7121ff7
Madan Mohan Reddy <119772090+1-MadanMohan@users.noreply.github.com>
2025-08-05 13:38:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-//your JS code here. If required.
+function isOdd(num) {
+  return num % 2 !== 0;
+}
+function isEven(num) {
+  return num % 2 === 0;
+}
+
+let promise = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve([1, 2, 3, 4]);
+  }, 3000);
+});
+
+promise.then((data) => {
+  let ans = data.filter(isOdd);
+
+  setTimeout(() => {
+    let element = document.getElementById("output");
+    element.value = ans.join(", "); // show as comma-separated
+  }, 1000); // 1 second delay
+}).then((data) => {
+  let ans = data.filter(isEven);
+
+  setTimeout(() => {
+    let element = document.getElementById("output");
+    element.value = ans.join(", "); // show as comma-separated
+  }, 2000); // 1 second delay
+});
